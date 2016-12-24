2b238600d6fb265ed9a09846a62c4cc2f3f4003e
unknown <av17lost@gmail.com>
2016-12-24 20:04:26 +0200

    Moved to jade and sass

--- config.rb
+++ config.rb
@@ -2,12 +2,15 @@ require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
+# coding: utf-8
 http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
 cache_path = '.tmp/.sass-cache'
+encoding = "utf-8"
+ preferred_syntax = :sass
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
